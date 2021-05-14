2ab40adacbfa82bbb1f9c86b358a438a05675155
ryu <goodrcg@gmail.com>
2021-05-14 16:36:36 +0900

    update lab2 3

--- Lab2/src/academy/pocu/comp3500/lab2/Queue.java
+++ Lab2/src/academy/pocu/comp3500/lab2/Queue.java
@@ -4,26 +4,33 @@ import academy.pocu.comp3500.lab2.datastructure.Node;
 
 public final class Queue {
     Node root;
+    Node front;
     int index;
 
     public Queue() {
         root = null;
+        front = null;
         index = -1;
     }
 
     public void enqueue(final int data) {
         ++index;
-//        root = LinkedList.insertAt(root, 0, data);
-        root = LinkedList.append(root, data);
+        root = LinkedList.prepend(root, data);
+        if (index == 0) {
+            front = root;
+        } else {
+            front.setNext(root);
+        }
     }
 
     public int peek() {
-        return LinkedList.getOrNull(root, 0).getData();
+//        return LinkedList.getOrNull(root, index).getData();
+        return front.getData();
     }
 
     public int dequeue() {
-        int data = LinkedList.getOrNull(root, 0).getData();
-        root = LinkedList.removeAt(root, 0);
+        int data = LinkedList.getOrNull(root, index).getData();
+        root = LinkedList.removeAt(root, index);
         --index;
         return data;
     }
