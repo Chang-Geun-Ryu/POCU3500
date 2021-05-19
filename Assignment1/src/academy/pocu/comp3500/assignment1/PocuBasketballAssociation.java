a5afe855a7729fb548b244787949c5f0875b21ce
ryu <goodrcg@gmail.com>
2021-05-19 15:51:41 +0900

    update ass1 log 2.4

--- Assignment1/src/academy/pocu/comp3500/assignment1/PocuBasketballAssociation.java
+++ Assignment1/src/academy/pocu/comp3500/assignment1/PocuBasketballAssociation.java
@@ -9,16 +9,91 @@ public final class PocuBasketballAssociation {
 
     public static void processGameStats(final GameStat[] gameStats, final Player[] outPlayers) {
 
+        quickSort(gameStats);
 
-        System.out.println(gameStats);
+        String name = "";
+        int gameCount = 0;
+        int totalPoint = 0;
+        int totalGoal = 0;
+        int totalGoalAttemp = 0;
+        int totalAssist = 0;
+        int totalPass = 0;
+
+        int playerIndex = 0;
+        for (int i = 0; i < gameStats.length; ++i) {
+            if (i == 0 || name.hashCode() == gameStats[i].getPlayerName().hashCode()) {
+                name = gameStats[i].getPlayerName();
+                totalPoint += gameStats[i].getPoints();
+                totalGoal += gameStats[i].getGoals();
+                totalGoalAttemp += gameStats[i].getGoalAttempts();
+                totalAssist += gameStats[i].getAssists();
+                totalPass += gameStats[i].getNumPasses();
+                gameCount += 1;
+            } else {
+                outPlayers[playerIndex].setName(name);
+                outPlayers[playerIndex].setPointsPerGame(totalPoint / gameCount);
+                outPlayers[playerIndex].setAssistsPerGame(totalAssist / gameCount);
+                outPlayers[playerIndex].setPassesPerGame(totalPass / gameCount);
+                outPlayers[playerIndex++].setShootingPercentage(100 * totalGoal / totalGoalAttemp);
+
+                name = gameStats[i].getPlayerName();
+                totalPoint = gameStats[i].getPoints();
+                totalGoal = gameStats[i].getGoals();
+                totalGoalAttemp = gameStats[i].getGoalAttempts();
+                totalAssist = gameStats[i].getAssists();
+                totalPass = gameStats[i].getNumPasses();
+                gameCount = 1;
+            }
+
+            if (outPlayers.length - 1 == playerIndex) {
+                outPlayers[playerIndex].setName(name);
+                outPlayers[playerIndex].setPointsPerGame(totalPoint / gameCount);
+                outPlayers[playerIndex].setAssistsPerGame(totalAssist / gameCount);
+                outPlayers[playerIndex].setPassesPerGame(totalAssist / gameCount);
+                outPlayers[playerIndex++].setShootingPercentage(100 * totalGoal / totalGoalAttemp);
+            }
+        }
     }
 
     public static Player findPlayerPointsPerGame(final Player[] players, int targetPoints) {
-        return null;
+        if (players.length < 1) {
+            return null;
+        }
+
+//        int left = 0;
+//        int right = players.length - 1;
+//        int findIndex = (left + right) / 2;
+//        Player p = players[findIndex];
+//
+//        while (left < right) {
+//            if (targetPoints == players[findIndex].getPointsPerGame()) {
+//                return players[findIndex];
+//            } else if (targetPoints < players[findIndex].getPointsPerGame()) {
+//                right = findIndex - 1;
+//                p = players[findIndex];
+//                findIndex = (left + right) / 2;
+//            } else {
+//                left = findIndex + 1;
+//                findIndex = (left + right) / 2;
+//            }
+//        }
+//
+//        int i = Math.abs(players[findIndex].getPointsPerGame() - targetPoints);
+//        int j = Math.abs(p.getPointsPerGame() - targetPoints);
+//
+//        if (i >= j) {
+//            return p;
+//        }
+
+        return findPlayer(players, targetPoints, true);
     }
 
     public static Player findPlayerShootingPercentage(final Player[] players, int targetShootingPercentage) {
-        return null;
+        if (players.length < 1) {
+            return null;
+        }
+
+        return findPlayer(players, targetShootingPercentage, false);
     }
 
     public static long find3ManDreamTeam(final Player[] players, final Player[] outPlayers, final Player[] scratch) {
@@ -33,4 +108,83 @@ public final class PocuBasketballAssociation {
         return -1;
     }
 
+    private static Player findPlayer(final Player[] players, int target, boolean findPoint) {
+        int left = 0;
+        int right = players.length - 1;
+        int findIndex = (left + right) / 2;
+        Player p = players[findIndex];
+        int current = 0;
+
+        while (left < right) {
+            current = getValue(players[findIndex], findPoint);
+
+            if (target == current) {
+                return players[findIndex];
+            } else if (target < current) {
+                right = findIndex - 1;
+                if (Math.abs(findPoint ? p.getPointsPerGame() - target : p.getShootingPercentage() - target) > Math.abs(current - target)) {
+                    p = players[findIndex];
+                }
+                findIndex = (left + right) / 2;
+            } else {
+                left = findIndex + 1;
+                if (Math.abs(findPoint ? p.getPointsPerGame() - target : p.getShootingPercentage() - target) > Math.abs(current - target)) {
+                    p = players[findIndex];
+                }
+                findIndex = (left + right) / 2;
+            }
+        }
+
+        int i = Math.abs(findPoint ? players[findIndex].getPointsPerGame() - target : players[findIndex].getShootingPercentage() - target);
+        int j = Math.abs(findPoint ? p.getPointsPerGame() - target : p.getShootingPercentage() - target);
+
+        if (i >= j) {
+            return p;
+        }
+
+        return players[findIndex];
+    }
+
+    private static int getValue(final Player p, boolean findPoint) {
+        return findPoint ? p.getPointsPerGame() : p.getShootingPercentage();
+    }
+
+    private static void quickSort(GameStat[] gameStats) {
+        quickSortRecursive(gameStats, 0, gameStats.length - 1);
+    }
+
+    private static void quickSortRecursive(GameStat[] gameStats, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int pivotPos = getPivotPos(gameStats, left, right);
+
+        quickSortRecursive(gameStats, left, pivotPos - 1);
+        quickSortRecursive(gameStats, pivotPos + 1, right);
+    }
+
+    private static int getPivotPos(GameStat[] gameStats, int left, int right) {
+        GameStat pivot = gameStats[right];
+        int index = left - 1;
+
+        for (int i = left; i < right; ++i) {
+            if (gameStats[i].getPlayerName().hashCode() < pivot.getPlayerName().hashCode()) {
+                ++index;
+                swap(gameStats, index, i);
+            }
+        }
+
+        index += 1;
+
+        swap(gameStats, index, right);
+
+        return index;
+    }
+
+    private static void swap(GameStat[] stat, int pos1, int pos2) {
+        GameStat temp = stat[pos1];
+        stat[pos1] = stat[pos2];
+        stat[pos2] = temp;
+    }
 }
--- Assignment1/src/academy/pocu/comp3500/assignment1/app/Program.java
+++ Assignment1/src/academy/pocu/comp3500/assignment1/app/Program.java
@@ -9,29 +9,73 @@ public class Program {
     public static void main(String[] args) {
         {
             GameStat[] gameStats = new GameStat[] {
-                    new GameStat("Player 1", 1, 13, 5, 6, 10, 1),
-                    new GameStat("Player 2", 2, 5, 2, 5, 0, 10),
-                    new GameStat("Player 1", 3, 12, 6, 9, 8, 5),
-                    new GameStat("Player 3", 1, 31, 15, 40, 5, 3),
-                    new GameStat("Player 2", 1, 3, 1, 3, 12, 2),
-                    new GameStat("Player 1", 2, 11, 6, 11, 9, 3),
-                    new GameStat("Player 2", 3, 9, 3, 3, 1, 11),
-                    new GameStat("Player 3", 4, 32, 15, 51, 4, 2),
-                    new GameStat("Player 4", 3, 44, 24, 50, 1, 1),
-                    new GameStat("Player 1", 4, 11, 5, 14, 8, 3),
-                    new GameStat("Player 2", 4, 5, 1, 3, 1, 9),
+//                    new GameStat("Player 1", 1, 13, 5, 6, 10, 1),
+//                    new GameStat("Player 2", 2, 5, 2, 5, 0, 10),
+//                    new GameStat("Player 1", 3, 12, 6, 9, 8, 5),
+//                    new GameStat("Player 3", 1, 31, 15, 40, 5, 3),
+//                    new GameStat("Player 2", 1, 3, 1, 3, 12, 2),
+//                    new GameStat("Player 1", 2, 11, 6, 11, 9, 3),
+//                    new GameStat("Player 2", 3, 9, 3, 3, 1, 11),
+//                    new GameStat("Player 3", 4, 32, 15, 51, 4, 2),
+//                    new GameStat("Player 4", 3, 44, 24, 50, 1, 1),
+//                    new GameStat("Player 1", 4, 11, 5, 14, 8, 3),
+//                    new GameStat("Player 2", 4, 5, 1, 3, 1, 9),
             };
 
             Player[] players = new Player[] {
-                    new Player(),
-                    new Player(),
-                    new Player(),
-                    new Player()
+//                    new Player(),
+//                    new Player(),
+//                    new Player(),
+//                    new Player()
             };
 
             PocuBasketballAssociation.processGameStats(gameStats, players);
 
-            System.out.println(players);
+            for (Player p: players) {
+                System.out.print("getName: " + p.getName());
+                System.out.print(", getPointsPerGame: " + p.getPointsPerGame());
+                System.out.print(", getAssistsPerGame: " + p.getAssistsPerGame());
+                System.out.print(", getPassesPerGame: " + p.getPassesPerGame());
+                System.out.println(", getShootingPercentage: " + p.getShootingPercentage());
+            }
+
+        }
+
+        {
+            Player[] players = new Player[] {
+                    new Player("Player 1", 1, 5, 1, 60),
+                    new Player("Player 2", 5, 2, 11, 31),
+                    new Player("Player 3", 7, 4, 7, 44),
+                    new Player("Player 4", 10, 10, 15, 25),
+                    new Player("Player 5", 11, 12, 6, 77),
+                    new Player("Player 6", 15, 0, 12, 61),
+                    new Player("Player 7", 16, 8, 2, 70)
+            };
+
+            Player player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 12); // player: Player 5
+            System.out.println("playerName: " + player.getName() + " , pointperGame: " + player.getPointsPerGame());
+            player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 5); // player: Player 2
+            System.out.println("playerName: " + player.getName() + " , pointperGame: " + player.getPointsPerGame());
+            player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 13); // player: Player 6
+            System.out.println("playerName: " + player.getName() + " , pointperGame: " + player.getPointsPerGame());
+        }
+        {
+            Player[] players = new Player[] {
+                    new Player("Player 4", 10, 10, 15, 25),
+                    new Player("Player 2", 5, 2, 11, 31),
+                    new Player("Player 3", 7, 4, 7, 44),
+                    new Player("Player 1", 1, 5, 1, 60),
+                    new Player("Player 6", 15, 0, 12, 61),
+                    new Player("Player 7", 16, 8, 2, 70),
+                    new Player("Player 5", 11, 12, 6, 77)
+            };
+
+            Player player = PocuBasketballAssociation.findPlayerShootingPercentage(players, 28); // player: Player 2
+            System.out.println("playerName: " + player.getName() + " , pointperGame: " + player.getShootingPercentage());
+            player = PocuBasketballAssociation.findPlayerShootingPercentage(players, 58); // player: Player 1
+            System.out.println("playerName: " + player.getName() + " , pointperGame: " + player.getShootingPercentage());
+            player = PocuBasketballAssociation.findPlayerShootingPercentage(players, 72); // player: Player 7\
+            System.out.println("playerName: " + player.getName() + " , pointperGame: " + player.getShootingPercentage());
         }
 //        {
 //            GameStat[] gameStats = new GameStat[] {
