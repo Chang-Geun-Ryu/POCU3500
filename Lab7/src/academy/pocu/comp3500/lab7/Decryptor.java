fffb4be7411fafb1a5db0b50e21ffaab0306d4dd
ryu <goodrcg@gmail.com>
2021-06-29 15:36:13 +0900

    update

--- Lab7/src/academy/pocu/comp3500/lab7/Decryptor.java
+++ Lab7/src/academy/pocu/comp3500/lab7/Decryptor.java
@@ -1,40 +1,23 @@
 package academy.pocu.comp3500.lab7;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Decryptor {
-
-    private TrieNode root = new TrieNode('\0', false);
+    private HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
 
     public Decryptor(final String[] codeWords) {
-
         for (String str : codeWords) {
-            String sortedWord = quickSort(str);
+            String sortWord = quickSort(str);
 
-            int index = 0;
-            addChars(root, sortedWord, str, 0);
+            if (hashMap.containsKey(sortWord)) {
+                hashMap.get(sortWord).add(str);
+            } else {
+                ArrayList<String> arrString = new ArrayList<>();
+                arrString.add(str);
+                hashMap.put(sortWord, arrString);
+            }
         }
-    }
-
-    private void addChars(TrieNode node, String sortedWord, String word, int index) {
-        if (sortedWord.length() == index) {
-            return;
-        }
-
-        char value = sortedWord.charAt(index);
-        boolean isEnd = index == sortedWord.length() - 1;
-
-        TrieNode child = node.getChild(value);
-        if (child == null) {
-            child = new TrieNode(value, isEnd);
-            node.addChildNode(child);
-        }
-
-        if (isEnd) {
-            child.addWord(word);
-        }
-
-        addChars(child, sortedWord, word, ++index);
     }
 
     public String[] findCandidates(final String word) {
@@ -44,36 +27,13 @@ public class Decryptor {
 
         String sortWord = quickSort(word);
 
-        HashMap<String, String> map = findWords(root, sortWord, 0);
-
-        if (map == null) {
+        if (hashMap.containsKey(sortWord)) {
+            ArrayList<String> arrString = hashMap.get(sortWord);
+            return arrString.toArray(new String[arrString.size()]);
+        } else {
             return new String[]{};
         }
-
-
-        return map.toArray(new String[map.size()]);
     }
-
-    private HashMap<String, String> findWords(TrieNode node, String words, int index) {
-        if (node.childMap.isEmpty()) {
-            return null;
-        } else if (index >= words.length()) {
-            return null;
-        }
-
-        char value = words.charAt(index);
-        TrieNode child =  node.childMap.get(value);
-        if (child == null) {
-            return null;
-        }
-
-        if (index == words.length() - 1 && child.isEndWord()) {
-            return child.words;
-        }
-
-        return findWords(child, words, ++index);
-    }
-
 
 
     private String quickSort(String word) {
@@ -94,19 +54,29 @@ public class Decryptor {
     }
 
     private int getPivot(char[] chars, int left, int right) {
+//        int i = left - 1;
+//
+//        for (int index = left; index < right; ++index) {
+//            if (chars[right] > chars[index]) {
+//                swap(chars, index, ++i);
+//            }
+//        }
+//
+//        swap(chars, right, i);
+//        return i;
         int pivot = right;
-        int i = left - 1;
+        int i = left;
 
         for (int index = left; index < right; ++index) {
             if (chars[pivot] > chars[index]) {
                 char temp = chars[index];
-                chars[index] = chars[++i];
-                chars[i] = temp;
+                chars[index] = chars[i];
+                chars[i++] = temp;
             }
         }
 
         char temp = chars[pivot];
-        chars[pivot] = chars[++i];
+        chars[pivot] = chars[i];
         chars[i] = temp;
 
         return i;
--- Lab7/src/academy/pocu/comp3500/lab7/DecryptorTest.java
+++ Lab7/src/academy/pocu/comp3500/lab7/DecryptorTest.java
@@ -2,21 +2,39 @@ package academy.pocu.comp3500.lab7;
 
 import java.util.HashMap;
 
-public class Decryptorquick {
+public class DecryptorTest {
+
     private TrieNode root = new TrieNode('\0', false);
 
-    public Decryptorquick(final String[] codeWords) {
-        for (String str : codeWords) {
-            String sortWord = quickSort(str);
+    public DecryptorTest(final String[] codeWords) {
 
-            if (hashMap.containsKey(sortWord)) {
-                hashMap.get(sortWord).add(str);
-            } else {
-                ArrayList<String> arrString = new ArrayList<>();
-                arrString.add(str);
-                hashMap.put(sortWord, arrString);
-            }
+        for (String str : codeWords) {
+            String sortedWord = quickSort(str);
+
+            int index = 0;
+            addChars(root, sortedWord, str, 0);
         }
+    }
+
+    private void addChars(TrieNode node, String sortedWord, String word, int index) {
+        if (sortedWord.length() == index) {
+            return;
+        }
+
+        char value = sortedWord.charAt(index);
+        boolean isEnd = index == sortedWord.length() - 1;
+
+        TrieNode child = node.getChild(value);
+        if (child == null) {
+            child = new TrieNode(value, isEnd);
+            node.addChildNode(child);
+        }
+
+        if (isEnd) {
+            child.addWord(word);
+        }
+
+        addChars(child, sortedWord, word, ++index);
     }
 
     public String[] findCandidates(final String word) {
@@ -26,13 +44,36 @@ public class Decryptorquick {
 
         String sortWord = quickSort(word);
 
-        if (hashMap.containsKey(sortWord)) {
-            ArrayList<String> arrString = hashMap.get(sortWord);
-            return arrString.toArray(new String[arrString.size()]);
-        } else {
+        HashMap<String, String> map = findWords(root, sortWord, 0);
+
+        if (map == null) {
             return new String[]{};
         }
+
+
+        return new String[]{};
     }
+
+    private HashMap<String, String> findWords(TrieNode node, String words, int index) {
+        if (node.childMap.isEmpty()) {
+            return null;
+        } else if (index >= words.length()) {
+            return null;
+        }
+
+        char value = words.charAt(index);
+        TrieNode child =  node.childMap.get(value);
+        if (child == null) {
+            return null;
+        }
+
+        if (index == words.length() - 1 && child.isEndWord()) {
+            return child.words;
+        }
+
+        return findWords(child, words, ++index);
+    }
+
 
 
     private String quickSort(String word) {
