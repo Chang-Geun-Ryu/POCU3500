57e72ec76595ad29e85ed12bcd52d84500b91d9a
ryu <goodrcg@gmail.com>
2021-07-25 00:04:12 +0900

    update

--- Lab10/src/academy/pocu/comp3500/lab10/Project.java
+++ Lab10/src/academy/pocu/comp3500/lab10/Project.java
@@ -2,77 +2,93 @@ package academy.pocu.comp3500.lab10;
 
 import academy.pocu.comp3500.lab10.project.Task;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 public class Project {
     public static List<String> findSchedule(final Task[] tasks, final boolean includeMaintenance) {
-
         HashMap<String, Task> discovered = new HashMap<>();
-        LinkedList<String> sortedList = new LinkedList<>();
+        LinkedList<String> visitTasks = new LinkedList<>();
 
         for (var t : tasks) {
             if (discovered.containsKey(t.getTitle())) {
                 continue;
             }
-            LinkedList<String> list = new LinkedList<>();
-            boolean temp = topologicalSortRecursive(0,
-                    t,
-                    t,
+
+            discovered.put(t.getTitle(), new Task(t.getTitle(), 0));
+
+            kosarajusRecursive(t,
                     discovered,
+                    visitTasks);
+        }
+
+        HashMap<String, Task> discoveredMap = new HashMap<>();
+        LinkedList<String> result = null;
+        for (var taskName : visitTasks) {
+            if (discoveredMap.containsKey(taskName)) {
+                continue;
+            }
+            Task task = discovered.get(taskName);
+
+            discoveredMap.put(taskName, task);
+
+            LinkedList<String> list = new LinkedList<>();
+
+            topologicalSortRecursive(task,
+                    discoveredMap,
                     list);
 
-            if (includeMaintenance) {
-                sortedList.addAll(list);
-            } else if (temp){
-                sortedList.addAll(list);
+            if (includeMaintenance == false && list.size() > 1) {
+                continue;
+            } else if (result == null) {
+                result = list;
+            } else {
+                list.addAll(result);
+                result = list;
             }
         }
 
-        return sortedList;
-
+        return result;
     }
 
-    private static boolean topologicalSortRecursive(int depth, Task startTask, Task task, HashMap<String, Task> discovered, LinkedList<String> linkedList) {
-        if (depth  > 0 && startTask.getTitle() == task.getTitle()) {
-            linkedList.addLast(task.getTitle());
-            return false;
-        }
-        if (discovered.containsKey(task.getTitle())) {
-            return true;
+    private static void kosarajusRecursive(Task task, HashMap<String, Task> discovered, LinkedList<String> visitTask) {
+        for (var t : task.getPredecessors()) {
+            if (discovered.containsKey(t.getTitle())) {
+                if (discovered.containsKey(task.getTitle())) {
+                    discovered.get(t.getTitle()).addPredecessor(discovered.get(task.getTitle()));
+                } else {
+                    discovered.get(t.getTitle()).addPredecessor(new Task(task.getTitle(), 0));
+                }
+                continue;
+            }
+
+            Task tempT = new Task(t.getTitle(), 0);
+            tempT.addPredecessor(task);
+            discovered.put(t.getTitle(), tempT);
+
+            kosarajusRecursive(t,
+                    discovered,
+                    visitTask);
         }
 
+        visitTask.addFirst(task.getTitle());
+    }
 
+    private static void topologicalSortRecursive(Task task, HashMap<String, Task> discovered, LinkedList<String> visitTask) {
         discovered.put(task.getTitle(), task);
 
-        boolean isAdd = true;
         for (var t : task.getPredecessors()) {
-            boolean temp = topologicalSortRecursive(depth + 1,
-                    startTask,
-                    t,
-                    discovered,
-                    linkedList);
-            if (temp == false) {
-                isAdd = false;
+            if (discovered.containsKey(t.getTitle())) {
+                continue;
             }
-//            linkedList.addLast(t.getTitle());
+
+            topologicalSortRecursive(t,
+                    discovered,
+                    visitTask);
         }
 
-        if (linkedList.size() > 0 && linkedList.getFirst() == task.getTitle()) {
-            int i = 0;
-        } else {
-            linkedList.addLast(task.getTitle());
-        }
-//        } else {
-//            linkedList.addLast(task.getTitle());
-//        }
-
-
-        return isAdd;
+        visitTask.addFirst(task.getTitle());
     }
-
 }
--- Lab10/src/academy/pocu/comp3500/lab10/app/Program.java
+++ Lab10/src/academy/pocu/comp3500/lab10/app/Program.java
@@ -10,6 +10,19 @@ public class Program {
 
     public static void main(String[] args) {
         {
+            Task[] tasks = createTasks3();
+            List<String> schedule = Project.findSchedule(tasks, true);
+
+            assert (schedule.size() == tasks.length);
+            assert (schedule.get(6).equals("6"));
+            assert (schedule.get(5).equals("1"));
+            assert (schedule.get(4).equals("4"));
+            assert (schedule.get(3).equals("5"));
+            assert (schedule.get(2).equals("0"));
+            assert (schedule.get(1).equals("3"));
+            assert (schedule.get(0).equals("2"));
+        }
+        {
             Task[] tasks = createTasks1();
 
             List<String> schedule = Project.findSchedule(tasks, true);
@@ -84,7 +97,42 @@ public class Program {
             assert (schedule.indexOf("D") < schedule.indexOf("G"));
             assert (schedule.indexOf("G") < schedule.indexOf("H"));
         }
+        {
+            Task[] tasks = createTasks2();
 
+            List<String> schedule = Project.findSchedule(tasks, true);
+
+            assert (schedule.size() == 9);
+            assert (schedule.get(0).equals("A"));
+            assert (schedule.get(1).equals("B"));
+            assert (schedule.get(2).equals("C"));
+            assert (schedule.indexOf("C") < schedule.indexOf("E"));
+            assert (schedule.indexOf("E") < schedule.indexOf("F"));
+            assert (schedule.indexOf("F") < schedule.indexOf("I"));
+
+            assert (schedule.indexOf("C") < schedule.indexOf("D"));
+            assert (schedule.indexOf("D") < schedule.indexOf("G"));
+            assert (schedule.indexOf("G") < schedule.indexOf("H"));
+        }
+        {
+            Task a = new Task("A", 15);
+            Task b = new Task("B", 12);
+            Task c = new Task("C", 11);
+
+            c.addPredecessor(b);
+            b.addPredecessor(a);
+            a.addPredecessor(c);
+
+            Task[] tasks = new Task[]{b, c, a};
+
+            List<String> schedule = Project.findSchedule(tasks, false);
+
+            assert (schedule.size() == 0);
+
+            schedule = Project.findSchedule(tasks, true);
+
+            assert (schedule.size() == 3);
+        }
 
 //
 
@@ -135,6 +183,53 @@ public class Program {
 //        a, b, c, d, e, f, g, h, i
         return new Task[]{
                 a, b, c, d, e, f, g, h, i
+        };
+    }
+    private static Task[] createTasks2() {
+        Task a = new Task("A", 12);
+        Task b = new Task("B", 7);
+        Task c = new Task("C", 10);
+        Task d = new Task("D", 9);
+        Task e = new Task("E", 8);
+        Task f = new Task("F", 11);
+        Task g = new Task("G", 14);
+        Task h = new Task("H", 13);
+        Task i = new Task("I", 6);
+
+        i.addPredecessor(f);
+        f.addPredecessor(e);
+        e.addPredecessor(b, c);
+        d.addPredecessor(c, h);
+        c.addPredecessor(b);
+        b.addPredecessor(a);
+        h.addPredecessor(g);
+        g.addPredecessor(d);
+//        a, b, c, d, e, f, g, h, i
+        return new Task[]{
+                a,a, b,b, c,c, d,d, e,e, f,f, g,g, h,h, i,i
+        };
+    }
+
+    private static Task[] createTasks3() {
+        Task a = new Task("0", 0);
+        Task b = new Task("1", 0);
+        Task c = new Task("2", 0);
+        Task d = new Task("3", 0);
+        Task e = new Task("4", 0);
+        Task f = new Task("5", 0);
+        Task g = new Task("6", 0);
+
+        a.addPredecessor(c, e);
+        b.addPredecessor(a);
+        c.addPredecessor(d);
+        d.addPredecessor(c);
+        e.addPredecessor(f);
+        f.addPredecessor(a);
+        g.addPredecessor(b);
+
+
+        return new Task[]{
+                a, b, c, d, e, f, g,
         };
     }
 }
